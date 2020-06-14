c27f53a815654f5a71e896f5b1d6706e43186478
Felipe Aliaga <f.aliagazamorano@gmail.com>
2020-06-14 19:59:48 -0400

    Clase 28

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,15 @@
+
+
+
+
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nombre?: string;
+  pagina_autor?: string;
+  instagram?: string;
+  facebook?: string;
+  twiter?: string;
+  tublr?: string;
+  equipo_trabajo?: any[];
+}
