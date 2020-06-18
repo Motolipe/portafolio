37f4b7572807ca2553fc919aead7305f29ee15bc
Felipe Aliaga <f.aliagazamorano@gmail.com>
2020-06-18 17:34:53 -0400

    Fin de la sección 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,22 +9,57 @@ export class ProductosService {
 
   cargando = true;
   producto: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient) {
 
     this.cargarProductos();
    }
 
-
+// forma para leer una api
   private cargarProductos() {
 
-    this.http.get('https://angular-pyme.firebaseio.com/productos_idx.json')
-    .subscribe( (resp: Producto[]) => {
+    return new Promise( ( resolve, rejects ) =>  {
+      this.http.get('https://angular-pyme.firebaseio.com/productos_idx.json')
+      .subscribe( (resp: Producto[]) => {
+        this.producto = resp;
+        this.cargando = false;
+        resolve();
+      });
 
-      console.log(resp);
-      this.producto = resp;
-      this.cargando = false;
     });
 
+    }
+
+  getProducto( id: string){
+   return this.http.get(`https://angular-pyme.firebaseio.com/productos/${ id }.json`);
   }
+
+  buscarProducto( termino: string ){
+      if ( this.producto.length === 0){
+        // cargar productos
+        this.cargarProductos().then( () => {
+          // ejecutar despues de tener los productos
+          // aplicar filtro
+          this.filtrarProductos( termino );
+        });
+      }else{
+        // aplicar el filtro
+        this.filtrarProductos( termino );
+      }
+  }
+  private filtrarProductos( termino: string ){
+
+    console.log( this.producto );
+    this.productosFiltrado = [];
+    termino = termino.toLocaleLowerCase();
+
+    this.producto.forEach( prod => {
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+      if (prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ){
+        this.productosFiltrado.push( prod );
+      }
+    });
+  }
+
 }
